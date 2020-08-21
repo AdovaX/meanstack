6dcb18987959ee68fa9ef7ab6664b8b3c046fe2e
adovax <mailstodeepakdev@gmail.com>
2020-08-21 19:42:55 +0530

    First born

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.componet';
+import { StudentsListComponent } from './components/students-list.component';
 
 const routes: Routes = [];
 
@@ -9,3 +11,5 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
